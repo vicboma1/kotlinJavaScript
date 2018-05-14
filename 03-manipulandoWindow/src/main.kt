7ab9d945bf42097beaae44d5e7ae43cce602a0dc
Victor Bolinches <vicboma1@users.noreply.github.com>
2018-05-14 13:14:13 +0200

    Update main.kt

--- 03-manipulandoWindow/src/main.kt
+++ 03-manipulandoWindow/src/main.kt
@@ -2,16 +2,11 @@
 import kotlin.browser.document
 import kotlin.browser.window
 
-/**
- * Created by vicboma on 08/05/2018.
- */
-
 fun main(args: Array<String>) {
 
     window.alert("Mensaje de alerta")
-
     window.confirm("Mensaje de confirmación")
-
+    
     document.body?.appendChild(
             document.createElement("button")
                     .apply {
